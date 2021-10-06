1ba7777997f443c9ccef86b3a5a74e9a93f1ce49
Plantoloz <70685898+Plantoloz@users.noreply.github.com>
2021-10-06 13:18:11 +0200

    working

--- ProjektApp/www/js/calender.js
+++ ProjektApp/www/js/calender.js
@@ -1,7 +1,39 @@
+// https://www.jquerypost.com/jquery-evo-calendar/
+myEvents = [
+    {
+        id: "required-id-1",
+        name: "New Year",
+        date: "Wed Jan 01 2020 00:00:00 GMT-0800 (Pacific Standard Time)",
+        type: "holiday",
+        everyYear: true
+    },
+    {
+        id: "required-id-2",
+        name: "Valentine's Day",
+        date: "Fri Feb 14 2020 00:00:00 GMT-0800 (Pacific Standard Time)",
+        type: "holiday",
+        everyYear: true,
+        color: "#222"
+    },
+    {
+        id: "required-id-3",
+        name: "Custom Date",
+        badge: "08/03 - 08/05",
+        date: ["August/03/2020", "August/05/2020"],
+        description: "Description here",
+        type: "event"
+    },
+];
+
 // Lädt Kalender
 $('#evoCalendar').evoCalendar({
     calendarEvents: myEvents
 });
-
+// Change Date Format 
+$('#evoCalendar').evoCalendar({
+    format: 'mm/dd/yyyy',
+    titleFormat: 'MM yyyy',
+    eventHeaderFormat: 'MM d, yyyy'
+  });
 
 console.log("Calendar loaded");
